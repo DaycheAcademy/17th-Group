1636a31d54b5aba8cc7bf7de9d1dc8cacf136d71
Mehdi Shokri Zadeh <31562498+Mehdi-Zadeh@users.noreply.github.com>
2023-08-08 19:48:45 +0330

    Delete hope-wiz.py

--- hope-wiz.py
+++
@@ -1,4 +0,0 @@
-for i in range(1, 101):
-    declear = 'hop-wiz' if i % 15 == 0 else 'wiz' if i % 3 == 0 else 'hop' if i % 3 == 0 else str(i)
-    print(declear)
-# I really don't know how to write this code in only one line
