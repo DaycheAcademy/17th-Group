1696f4503453a0f534186497488d66ecf1d479eb
Behdad <kh.behdad.1999@Gmail.com>
2023-08-15 14:28:13 +0330

    creating Name Dir

---
+++ Behdad KHosraviani/RPS.py
@@ -0,0 +1,7 @@
+# defining the core functions of the game
+
+def wins(computer_choice , human_choice):
+    if computer_choice == human_choice:
+        print('drawing')
+    elif computer_choice == "ss":
+        if human_choice == ""
--- test.py
+++ test.py
@@ -5,3 +5,12 @@ print(myit)
 print(next(myit))
 print(next(myit))
 print(next(myit))
+
+
+print(40 * "++")
+
+x = 0
+if not x:
+    print("This will return True")
+else:
+    print("This will return False")
