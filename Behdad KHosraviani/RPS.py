0d87140852f9f000d62d5b388a4e0ef74e92f16c
Behdad <kh.behdad.1999@Gmail.com>
2023-08-15 17:47:11 +0330

    almost done

--- Behdad KHosraviani/RPS.py
+++ Behdad KHosraviani/RPS.py
@@ -1,3 +1,5 @@
+from random import choice
+
 # we are a polite developer, lets greet the player
 
 welcome_text = ''' WELCOME to RPS game,
@@ -9,78 +11,135 @@ welcome_text = ''' WELCOME to RPS game,
 print(welcome_text)
 print('********************************************')
 
-# defining the core functions of the game
-
-computer_score = 0
-palyer_score = 0
-
-def who_wins(computer_choice, human_choice):
-    if computer_choice == human_choice:
-        print('that was a draw , no point for either of us')
-    elif computer_choice == "ss":
-        if human_choice == "rk":
-            print('human wins')
-        else:
-            print('computer')
-    elif computer_choice == "rk":
-        if human_choice == "pp":
-            print('human wins')
-        else:
-            print('computer')
-    else:
-        if human_choice == "ss":
-            print('human wins')
-        else:
-            print('computer')
-
 
 # now we have the core function, lets take input from human
 quite_list = ['q', "quit", "Q", 'Quit']
 
-
-while True:
-    time_play = input('how much rounds yoy may play? :(type quit to end) ').strip()
-    if time_play.isdigit():
-        time_play = int(time_play)
-        break
-    elif time_play in quite_list:
-        break
-    else:
-        print('please enter a number.(type quit to end) ')
-
-print(time_play)
-
-# I would prefer to determine the computer choice just now
-
-from random import choice
-
-RPS = ['Rock','Paper','Scissor']
-computer_choice = choice(RPS)
-print(computer_choice)
-
-# now we get to the hard part, taking input from the hurman
-
-human_choice_list = ['Rock', 'Paper', 'Scissor', 'r', 'R', 'p', 'P', 's', 'S', 'ss', 'rock', 'paper', 'scissor']
-
-if time_play not in quite_list :
-    breaker = False
-    while breaker == False:
-        human_choice = input('Now try your chance! which one?(type quit to end)').split(sep=' ')
-        human_choice.append("fix bug")
-        for i in human_choice:
-            if i in quite_list:
-                print('game is done')
-                q = True
-                break
-            if breaker == True:
-                break
-            for j in human_choice_list:
-                if j == i:
-                    human_choice = i
-                    breaker = True
-                    break
+def round_play():
+    while True:
+        time_play = input('how much rounds yoy may play? :(type quit to end) ').strip()
+        if time_play.isdigit():
+            print('****************************')
+            print('HaHa So you want to play pro!')
+            print('Ok Darling :)))))))) ')
+            print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+            return int(time_play)
+        elif time_play in quite_list:
+            return 'q'
         else:
-            print('Come on! don\'t play with me! enter a valid choice!')
+            print('please enter a number.(type quit to end) ')
+
+
+# now we get to the hard part, taking input from the human
+
+human_choice_list = ['Rock', 'rock', 'r', 'R', 'Paper', 'paper', 'p', 'P', 'Scissor', 'scissor', 'sc', 'S', 's']
+def take_input():
+    if time_play not in quite_list :
+        while True:
+            human_choice = input('Now try your chance! which one?(type quit to end)  ').split(sep=' ')
+            human_choice.append("fix the bug")
+            for i in human_choice:
+                if i in quite_list:
+                    print('Game is Done!')
+                    return 'quit'
+                for j in human_choice_list:
+                    if j == i:
+                        human_choice = i     # here we take the naive input
+                        if human_choice in human_choice_list[0:4]:
+                            return human_choice_list[0]
+                        elif human_choice in human_choice_list[4:8]:
+                            return human_choice_list[4]
+                        else:
+                            return human_choice_list[8]     # and here we make it clean
+            else:
+                print('Come on! don\'t play with me! Enter a valid choice!')
+
+    else:
+        print('Game is Done!')
+
+
+# defining the core functions of the game
+
+time_play = round_play()
+if time_play == 'q':
+    print('OK we play later! good luck')
+    time_play = 0
+
+computer_score = 0
+player_score = 0
+
+while computer_score + player_score != time_play :
+    RPS = ['Rock', 'Paper', 'Scissor']
+    computer_choice = choice(RPS)
+    human_choice = 'Rock'
+    print(human_choice)
+    if human_choice in quite_list:
+        break
+    if computer_choice == human_choice:
+        print('that was a draw , no point for either of us')
+    elif computer_choice == "Scissor":
+        if human_choice == "Rock":
+            print('Ok you win')
+            player_score = + 1
+            print('''*************************
+                        *****************************''')
+            print('I picked {} and you picked {}'.format(computer_choice, human_choice))
+            print('your score is: ', player_score)
+            print('................................')
+            print('My score is: ', computer_score)
+        else:
+            print('I win !!!!!!!!')
+            computer_score = + 1
+            print('''*************************
+                                    *****************************''')
+            print('I picked {} and you picked {}'.format(computer_choice, human_choice))
+            print('your score is: ', player_score)
+            print('................................')
+            print('My score is: ', computer_score)
+
+    elif computer_choice == "Rock":
+        if human_choice == "Paper":
+            print('you got lucky, you win!')
+            player_score = + 1
+            print('''*************************
+                                    *****************************''')
+            print('I picked {} and you picked {}'.format(computer_choice, human_choice))
+            print('your score is: ', player_score)
+            print('................................')
+            print('My score is: ', computer_score)
+        else:
+            print('It happens, sometime you loose in life')
+            computer_score = + 1
+            print('''*************************
+                                    *****************************''')
+            print('I picked {} and you picked {}'.format(computer_choice, human_choice))
+            print('your score is: ', player_score)
+            print('................................')
+            print('My score is: ', computer_score)
+    else:
+        if human_choice == "Scissor":
+            print('be proud, you win!')
+            player_score = + 1
+            print('''*************************
+                                    *****************************''')
+            print('I picked {} and you picked {}'.format(computer_choice, human_choice))
+            print('your score is: ', player_score)
+            print('................................')
+            print('My score is: ', computer_score)
+        else:
+            print('Yes I win!')
+            computer_score = + 1
+            print('''*************************
+                                    *****************************''')
+            print('I picked {} and you picked {}'.format(computer_choice, human_choice))
+            print('your score is: ', player_score)
+            print('................................')
+            print('My score is: ', computer_score)
+    print('================================================================')
+
+
+
+
 
 
 
