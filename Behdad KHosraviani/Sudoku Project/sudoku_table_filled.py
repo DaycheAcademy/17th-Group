c50da31302c4d371ddbb6c40dad05d4542006ba5
Behdad <kh.behdad.1999@Gmail.com>
2023-10-06 15:06:59 +0330

    filling the table with random numbers

---
+++ Behdad KHosraviani/Sudoku Project/sudoku_table_filled.py
@@ -0,0 +1,34 @@
+import random
+import turtle
+import sudoku_table as st
+
+
+# first we need a function to  generate random numbers from 1 to 10
+
+def random_filler():
+    num = random.randint(1, 11)
+    turtle.write(num,
+                 move=False, align='center',
+                 font=('Arial', 15, 'normal'))
+
+
+def fill_with_number():
+    if st.validation_for_running == 1:
+        turtle.up()
+        turtle.setpos(252 - 28, -280 + 20)
+        xpos = turtle.position()[0]
+        ypos = turtle.position()[1]
+        for j in range(9):
+            for i in range(9):
+                turtle.setpos(xpos - (i * 56), ypos + (j * 56))
+                random_filler()
+    else:
+        print('first draw the table')
+
+
+st.drow_soduko_table()
+print(turtle.position())
+
+fill_with_number()
+
+turtle.done()
